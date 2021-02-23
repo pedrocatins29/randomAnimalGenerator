fc1633f0f5598caa29f56819a0aba07e3bfee70a
pedro.souza <pedro.souza@life.net.br>
2021-02-23 16:52:22 -0300

    isso ai mesmo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [currentImage, setCurrentImage] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDogs = async () => {
+    try {
+      setLoading(true);
+      const result = await fetch("https://dog.ceo/api/breeds/image/random");
+      const parsedResult = await result.json();
+
+      if (parsedResult.status === "success") {
+        setCurrentImage(parsedResult.message);
+        setLoading(false);
+      } else {
+        alert("aconteceu algo de errado com o seu request 😔");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const fetchCats = async () => {
+    try {
+      setLoading(true);
+      const result = await fetch("https://aws.random.cat/meow");
+      const parsedResult = await result.json();
+      setCurrentImage(parsedResult.file);
+      setLoading(false);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="App">
+        <button onClick={() => fetchCats()} className="buttonCat">
+          Gerar um gato
+        </button>
+        <button onClick={() => fetchDogs()} className="buttonDog">
+          Gerar um cachorro
+        </button>
+      </div>
+
+      <span>{loading ? "carregando ..." : null}</span>
+
+      {currentImage ? (
+        <img src={currentImage} width="400px" height="400px" />
+      ) : (
+        <img
+          src="https://image.flaticon.com/icons/png/512/130/130864.png"
+          width="400px"
+          height="400px"
+        />
+      )}
     </div>
   );
 }
